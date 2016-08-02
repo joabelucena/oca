568c1f6e112828d360fbb996b3af9c80e0b27bf4
Joabe Lucena <joabelucena@gmail.com>
2016-08-01 21:50:57 -0300

    enthuware: Test4

---
+++ src/enthuware/Test4.java
@@ -0,0 +1,30 @@
+package enthuware;
+
+import static lib.TextElements.*;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		
+		{
+//			short s = new Short(1);
+		}
+		
+		separe();
+		
+		{
+//			{int a = b = c = 100;}
+			
+			{int a,b,c,d =100; System.out.println(d);}
+		}
+		
+		separe();
+		
+		{
+			System.out.println("a".compareTo("A"));
+		}
+		
+		
+	}
+
+}
